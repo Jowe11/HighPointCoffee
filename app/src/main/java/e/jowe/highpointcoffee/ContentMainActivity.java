268c321ae45a481e93523e367f8877e5fb189451
Joseph ROWE <jowe@eleven-rules.xyz>
2017-12-17 00:37:17 -0700

    Initial commit

---
+++ app/src/main/java/e/jowe/highpointcoffee/ContentMainActivity.java
@@ -0,0 +1,13 @@
+package e.jowe.highpointcoffee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
